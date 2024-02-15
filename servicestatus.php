cc361c4b21202ecf761ba898da858de2b23d1838
hiddensetup <franka@dublin.com>
2024-02-15 02:30:25 -0300

    modifications landing page

--- servicestatus.php
+++ servicestatus.php
@@ -1,18 +1,20 @@
 <?php
 header('Content-Type: application/json');
 
-function generateRandomPercentage($min, $max) {
+function generateRandomPercentage($min, $max)
+{
     return number_format(mt_rand($min, $max) / 10, 2);
 }
 
-function getCurrentDateTime() {
+function getCurrentDateTime()
+{
     $dateTime = new DateTime("now", new DateTimeZone("America/Argentina/Buenos_Aires")); // Adjust timezone accordingly
     return $dateTime->format('M d Y H:i:s') . ' GMT-3';
 }
 
-$usaStatus = generateRandomPercentage(989, 1000) . '% Funcionando';
-$brasilStatus = generateRandomPercentage(979, 990) . '% Funcionando';
-$generalStatus = generateRandomPercentage(998, 1000) . '% Funcionando';
+$usaStatus = generateRandomPercentage(989, 1000) . '%  OK!';
+$brasilStatus = generateRandomPercentage(979, 990) . '%  OK!';
+$generalStatus = generateRandomPercentage(998, 1000) . '%  OK!';
 
 echo json_encode([
     'usa' => $usaStatus,
@@ -20,4 +22,3 @@ echo json_encode([
     'general' => $generalStatus,
     'serverTime' => getCurrentDateTime(),
 ]);
-?>
